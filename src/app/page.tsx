ce592568af1d0b9dc936055bdbdae91e0fe63042
jpsileskh <jpsileskh@hotmail.com>
2025-11-03 21:10:46 -0300

    msg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>João Paulo Teixeira de Siles</h1>
-      <h3>Desenvolvedor Calvo e Gostoso</h3>
+      <h1>João Paulo Teixeira de Siles</h1>      
     </div>
   );
 }
